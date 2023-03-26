8eadfda30a4630d2f91157d6714327a84ade8bb9
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-03-25 17:34:28 -0700

    Pseudocode added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,4 @@
 // list of all questions, choices, and answers
-
 var questions = [
     {
       title: 'Commonly used data types DO NOT include:',
@@ -34,5 +33,24 @@ var questions = [
       answer: 'console.log',
     },
   ];
-
   
+
+//   Pseudocode
+/*
+1. WHEN I click the START QUIZ button:
+    a.timer will begin
+    b.questions will load up
+
+2. WHEN I answer a question:
+    a.Correctly:
+        i. i am presented with another question
+    b.Incorrectly:
+        i. time is subtracted from the clock
+        ii. i am presented with another question
+
+3. WHEN all questions are answered OR the timer reaches 0:
+    a.the game is over
+
+4. WHEN the game is over:
+    a. i can save my initials and my score
+*/
