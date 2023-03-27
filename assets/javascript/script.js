ad97cca66a88503d6de767e8e7a953869ca7cc76
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-03-26 20:31:36 -0700

    js timer finished. displaying questions function started

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,40 +1,40 @@
 // list of all questions, choices, and answers
 var questions = [
-    {
-      title: 'Commonly used data types DO NOT include:',
-      choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: 'alerts',
-    },
-    {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: 'parentheses',
-    },
-    {
-      title: 'Arrays in JavaScript can be used to store ____.',
-      choices: [
-        'numbers and strings',
-        'other arrays',
-        'booleans',
-        'all of the above',
-      ],
-      answer: 'all of the above',
-    },
-    {
-      title:
-        'String values must be enclosed within ____ when being assigned to variables.',
-      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-      answer: 'quotes',
-    },
-    {
-      title:
-        'A very useful tool used during development and debugging for printing content to the debugger is:',
-      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-      answer: 'console.log',
-    },
-  ];
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
+];
 
-  //   Pseudocode
+//   Pseudocode
 /*
 1. WHEN I click the START QUIZ button: (eventlistener will be needed on button)
     a.timer will begin
@@ -62,35 +62,43 @@ var questions = [
 */
 
 // ----------------Query Selectors--------------------------
-var startQuizButton = document.querySelector('#startQuiz');
-var displayedTime = document.querySelector('#displayTime');
+var startQuizButton = document.querySelector("#startQuiz");
+var displayedTime = document.querySelector("#displayTime");
+var quizDiv = document.querySelector("#quizDiv");
 
 // ----------------Global Variables-------------------------
 // number reliant on amount of questions
 var timeLeft = questions.length * 15; // 75 (for now)
 
-
 // ------------------------------------------------------------------------------
 // (1)event listener added to START QUIZ button in html.
 //(a) timer function created and placed within click event
-startQuizButton.addEventListener("click", function (){
-  function countDown(){
-    const minutes = Math.floor(timeLeft/60);
+startQuizButton.addEventListener("click", function () {
+  //(a) timer function created and placed within click event
+  // countDown function
+  function countDown() {
+    const minutes = Math.floor(timeLeft / 60);
     var seconds = timeLeft % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
     displayedTime.innerHTML = `${minutes}:${seconds}`;
     timeLeft--;
-  };
+    if (timeLeft <= 0) {
+      displayedTime.textContent = "Time's up!";
+    }
+  }
   setInterval(countDown, 1000);
+  // b.questions will be presented
 
-  
 });
+
+
+
 
 
 // b.questions will be presented
 
-function quizQuestions (){
-  if (timeLeft > 0) {
-    
+function displayQuestions() {
+  if (timeLeft >= 1) {
+
   }
-};
+}
